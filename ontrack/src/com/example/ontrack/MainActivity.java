5d81c4962c7900ae4871bcc22b8d25b67baf881e
Daniel Dalton <daniel.dalton10@gmail.com>
2014-11-08 21:44:50 +1100

    Simplify - remove all speech - use vibrations 3 vibrations means back on course two vibrations mean go left 1 vibration means go right. Intensity means magnitude of degree off course

--- ontrack/src/com/example/ontrack/MainActivity.java
+++ ontrack/src/com/example/ontrack/MainActivity.java
@@ -6,8 +6,6 @@ import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,57 +13,21 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    private static int MINIMUM_STABILITY_COUNT = 0;
-    private static float STABILITY_MARGIN = 3;
-    private static float COURSE_MARGIN = 10;
-    private static long SPOKEN_TIME_OFFSET = 5000;
-    private static boolean SPEECH_ENABLED = true;
-    private static long STABILITY_TIME_OFFSET = 500;
+    private static final float STABILITY_MARGIN = 3;
+    private static final float COURSE_MARGIN = 10;
     private static final long UPDATE_OFFSET = 3000;
-    
+    private static final long STABILITY_TIME_OFFSET = 300;
+
     private long lastUpdate = 0;
-    // private static long[] VIBE_PATTERN = { 0, 500, 0 };
-
-    private enum ClockDirection {
-        ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), ELEVEN(11), TWELVE(
-                12);
-
-        private final int value;
-
-        ClockDirection(int value) {
-            this.value = value;
-        }
-
-        public int value() {
-            return value;
-        }
-
-        public static ClockDirection valueOf(int value) {
-            if (value == 0) {
-                return ClockDirection.TWELVE;
-            }
-            for (ClockDirection clockDirection : values()) {
-                if (clockDirection.value() == value) {
-                    Log.d("ONTRACK", "Setting the direction to be " + clockDirection.value());
-                    return clockDirection;
-                }
-            }
-            throw new RuntimeException("Invalid value " + value);
-        }
-    }
-
     private Vibrator vibe;
-    private TextToSpeech tts;
     private Button setButton;
-    private boolean onCourse;
-    private int stabilityCount = 0;
-    private long lastSpokenUpdate = 0;
     private long lastStableHeadingTime = 0;
-    private boolean newDirection;
     private float lastStableHeading = 0;
     private float currentHeading;
     private float direction;
     private boolean trackingDirection;
+    private boolean alreadyBackOnTrack;
+    
     private SensorManager sensorManager;
     /* TODO: remove warnings use modern compass method */
     private final SensorListener mListener = new SensorListener() {
@@ -86,24 +48,19 @@ public class MainActivity extends Activity {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensorManager.registerListener(mListener, SensorManager.SENSOR_ORIENTATION,
                 SensorManager.SENSOR_DELAY_GAME);
-        tts = new TextToSpeech(getApplicationContext(), null);
         setButton = (Button) findViewById(R.id.btn_set_compass);
         setButton.setEnabled(false);
         vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         if (savedInstanceState != null) {
             direction = savedInstanceState.getFloat("direction");
             trackingDirection = savedInstanceState.getBoolean("trackingDirection");
-            onCourse = savedInstanceState.getBoolean("onCourse");
-            if (!onCourse) {
-                // vibe.vibrate(VIBE_PATTERN, 0);
-            }
+            vibrateOffCourse();
         }
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putBoolean("trackingDirection", trackingDirection);
-        savedInstanceState.putBoolean("onCourse", onCourse);
         savedInstanceState.putFloat("direction", direction);
         super.onSaveInstanceState(savedInstanceState);
     }
@@ -111,7 +68,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         vibe.cancel();
-        tts.shutdown();
         sensorManager.unregisterListener(mListener);
         super.onDestroy();
     }
@@ -119,7 +75,7 @@ public class MainActivity extends Activity {
     public void handleDirection() {
         boolean reliableReading = false;
         if (Math.abs(currentHeading - lastStableHeading) < STABILITY_MARGIN) {
-            if (lastStableHeadingTime + STABILITY_TIME_OFFSET > System.currentTimeMillis()) {
+            if (lastStableHeadingTime + STABILITY_TIME_OFFSET < System.currentTimeMillis()) {
                 reliableReading = true;
                 setButton.setEnabled(true);
                 setButton.setText("Set " + Math.round(currentHeading) + " degrees");
@@ -134,67 +90,12 @@ public class MainActivity extends Activity {
         }
     }
 
-    public void processDirection() {
-        if (Math.abs(currentHeading - lastStableHeading) <= STABILITY_MARGIN) {
-            ++stabilityCount; // The stability reading increases each time
-        } else { // reset reading - unstable
-            lastStableHeading = currentHeading; // Reset for new readings
-            stabilityCount = 0;
-            newDirection = true;
-            if (trackingDirection && onCourse && Math.abs(currentHeading - direction) > COURSE_MARGIN) {
-                // moved off course
-                onCourse = false;
-                // vibe.vibrate(VIBE_PATTERN, 0);
-            }
-        }
-
-        // Check that stability is stable and that we have a new direction i.e.
-        // don't want to keep speaking stuff if the compass is remaining still
-        if (newDirection && stabilityCount >= MINIMUM_STABILITY_COUNT) {
-            newDirection = false;
-            Log.d("COMPASS", "heading is: " + currentHeading);
-            setButton.setEnabled(true);
-            setButton.setText("Set " + Math.round(currentHeading) + " degrees");
-            if (trackingDirection && Math.abs(currentHeading - direction) > COURSE_MARGIN) {
-                // stable reading - we are still off course
-                directBackOnCourse();
-            } else if (trackingDirection && !onCourse
-                    && Math.abs(currentHeading - direction) <= COURSE_MARGIN) {
-                // We have just moved back on course
-                onCourse = true;
-                vibe.cancel();
-                Log.d("COMPASS", "Back on course!");
-                tts.speak("Back on course!", 0, null);
-                lastSpokenUpdate = 0;
-            }
-        }
-    }
-
     public void setDirection(View view) {
         this.direction = currentHeading;
         this.trackingDirection = true;
-        onCourse = true;
         String message = "Set your direction to: " + Math.round(currentHeading) + " degrees";
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
         vibe.cancel();
-    }
-
-    private void directBackOnCourse() {
-        long currentTime = System.currentTimeMillis();
-        if (!SPEECH_ENABLED || lastSpokenUpdate + SPOKEN_TIME_OFFSET > currentTime) {
-            return; // Don't want to constantly speak wait some time since last
-                    // utterance
-        }
-        lastSpokenUpdate = currentTime;
-        int toHead = getDegreeOffCourse();
-        // vibrateOffCourse(getClockDirectionToHeading());
-        // values < 0 are left of the direction we want to go values > 0 are to
-        // the right of direction we want to go
-        /*
-         * if (toHead < 0) { tts.speak("Go left " + Math.abs(toHead) +
-         * " degrees", 0, null); } else { tts.speak("Go right " + toHead +
-         * " degrees", 0, null); }
-         */
     }
 
     private int getDegreeOffCourse() {
@@ -217,43 +118,30 @@ public class MainActivity extends Activity {
         }
     }
 
-    private ClockDirection getClockDirectionToHeading() {
-        int direction = Math.round((this.direction - currentHeading) / 30);
-        if (direction < 0) {
-            direction = 12 - Math.abs(direction);
-        }
-        return ClockDirection.valueOf(direction);
-    }
-
     private void vibrateOffCourse() {
         if (System.currentTimeMillis() < lastUpdate + UPDATE_OFFSET) {
             return;
         }
-        lastUpdate = System.currentTimeMillis();
+
         int offCourse = getDegreeOffCourse();
         long duration = 10 * Math.abs(offCourse);
-        vibe.cancel ();
-        if (offCourse < 0) {
-            long[] pattern = {0, 300, 300, duration, 0};
+        lastUpdate = System.currentTimeMillis() + duration;
+        vibe.cancel();
+        if (Math.abs(offCourse) < COURSE_MARGIN) {
+            if (!alreadyBackOnTrack) {
+                alreadyBackOnTrack = true;
+                long[] pattern = { 0, 300, 300, 300, 300, 300, 0 };
+                vibe.vibrate(pattern, -1);
+            }
+        } else if (offCourse < 0) {
+            alreadyBackOnTrack = false;
+            long[] pattern = { 0, 300, 300, duration, 0 };
             vibe.vibrate(pattern, -1);
         } else {
-            long[] pattern = {0, duration, 0};
+            alreadyBackOnTrack = false;
+            long[] pattern = { 0, duration, 0 };
             vibe.vibrate(pattern, -1);
         }
-    }
-
-    private int buildPattern(long[] pattern, int start, long startDelay, long on, long off, int repeats) {
-        pattern[start] = startDelay;
-        for (int i = start + 1; i < start + 2 * repeats; i += 2) {
-            pattern[i] = on;
-            pattern[i + 1] = off;
-        }
-        StringBuilder sb = new StringBuilder("{");
-        for (long p : pattern) {
-            sb.append(String.valueOf(p)).append(",");
-        }
-        Log.d("ONTRACK", sb.substring(0, sb.length() - 1) + "}");
-        return start + 2 * repeats;
     }
 
     @Override
